65d8d10e7f0cac920e78dd2dae172cf65e54ce7f
nicole <nicole.tpelentir@hotmail.com>
2023-09-27 09:23:58 -0300

    4

--- sus/html/login.php
+++ sus/html/login.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div id="area">
-        <form id="login" autocomplete="off" action="/src/teste.php" method="POST">
+        <form id="login" autocomplete="off" action="/html/src/teste.php" method="POST">
             <fieldset>
                 <div class="classecpf">
                     <label id="cpf" for="floatingInput">CPF</label>
@@ -30,5 +30,6 @@
                 </div>
             </fieldset>
         </form>
+        <?php print_r($_POST); ?>
 </body>
 </html>
--- sus/html/src/teste.php
+++ sus/html/src/teste.php
@@ -1,14 +1,15 @@
 <?php
 //session_start();
+
 if (isset($_POST['submit']) && !empty($_POST['cpf']) && !empty($_POST['senha'])){//entra
-    include_once('/Banco/src/conexao.php');
+    include_once('../Banco/src/conexao.php');
 
     $cpf=$_POST['cpf'];
     $senha=$_POST['senha'];
-    print_r($_POST);
+    
 
 }else{//não entra
-    header('Location:index.html');
+    header('Location:../index.php');
 
 }
 
