399565f5c908e9be0273ed0004259b794c24f7ec
Renan Parise <renan.parise.ifc@gmail.com>
2023-10-29 12:20:44 -0300

    login

--- sus/html/login.php
+++ sus/html/login.php
@@ -1,46 +1,57 @@
+<?php
+
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="../css/stylelogin.css">
+    <title>Login conSUStar</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php
-    if (isset($_SESSION['msg'])) :
-      $msg = $_SESSION['msg'];
-      unset($_SESSION['msg']);
-    ?>
-    <div id="area">
-    <?= $msg ?>
-    <?php
-    endif;
-    ?>
-        <form id="login" autocomplete="off" action="efetua_login.php" method="POST">
-            <fieldset>
-                <div class="classecpf">
-                    <label id="cpf" for="floatingInput">CPF</label>
-                    <br>
-                    <input id="cpfInput" type="text" class="form-control" id="floatingInput" placeholder="111.111.111-11" name="cpf" maxlength="11" oninput="validar(this)">   
-                </div>
-                <div class="classesenha">
-                    <label id="senha" for="floatingPassword">Senha</label>
-                    <br>
-                    <input id="senha" type="password" class="form-control" id="floatingPassword" placeholder="Senha" name="senha">
-                 
-                </div>
-              
-                <input id="bEntrar" class="btn btn-primary" type="submit">Entrar</input>
-                
-                <div class="funcionais">
-                    <a id="esquecisenha" href="botar o link que vai ser redirecionado aq" class="btn btn-primary disabled" tabindex="-1" role="button" aria-disabled="true">Esqueci minha senha</a>
-                    <a id="semcad" href="botar o link que vai ser redirecionado aq" class="btn btn-primary disabled" tabindex="-1" role="button" aria-disabled="true">Não possuo cadastro</a>
-                </div>
-            </fieldset>
-        </form>
-        <?php print_r($_POST); ?>
-
-        <script src="../js/index.js"></script>
+    <div class="container">
+        <?php
+        if (isset($_POST["login"])) {
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+            require_once "db.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: index.php");
+                    die();
+                }else{
+                    echo "<div class='alert alert-danger'>Senha incorreta. Tente novamente.</div>";
+                }
+            }else{
+                echo "<div class='alert alert-danger'>Email não existe.</div>";
+            }
+        }
+        ?>
+      <form action="login.php" method="post">
+        <div class="form-group">
+            <input type="email" placeholder="Enter Email:" name="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="password" placeholder="Enter Password:" name="password" class="form-control">
+        </div>
+        <div class="form-btn">
+            <input type="submit" value="Login" name="login" class="btn btn-primary">
+        </div>
+      </form>
+     <div><p>Não tem conta? <a href="registro.php">Registro</a></p></div>
+    </div>
 </body>
 </html>
---
+++ sus/html/registro.php
@@ -0,0 +1,124 @@
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro conSUStar</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php
+        if (isset($_POST["submit"])) {
+           $fullName = $_POST["fullname"];
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+           $passwordRepeat = $_POST["repeat_password"];
+           $cpf = $_POST["cpf"];
+           $telefone = $_POST["telefone"];
+           $numCarteira = $_POST["numCarteira"];
+           $endereco = $_POST["endereco"];
+           $numero = $_POST["numero"];
+           $cidade = $_POST["cidade"];
+           $estado = $_POST["estado"];
+           $cep = $_POST["cep"];
+           
+           $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+           $errors = array();
+           
+           if (empty($fullName) OR empty($email) OR empty($password) OR empty($passwordRepeat) OR empty($cpf) OR empty($telefone) OR empty($numCarteira) OR empty($endereco) OR empty($numero) OR empty($cidade) OR empty($estado) OR empty($cep)) {
+            array_push($errors,"Você deve preencher todos os campos.");
+           }
+           if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            array_push($errors, "Email não é válido.");
+           }
+           if (strlen($password)<8) {
+            array_push($errors,"A senha deve ter no mínimo 8 caracteres.");
+           }
+           if ($password!==$passwordRepeat) {
+            array_push($errors,"A senha não é igual.");
+           }
+           require_once "db.php";
+           $sql = "SELECT * FROM users WHERE email = '$email'";
+           $result = mysqli_query($conn, $sql);
+           $rowCount = mysqli_num_rows($result);
+           if ($rowCount>0) {
+            array_push($errors,"Email já existe!");
+           }
+           if (count($errors)>0) {
+            foreach ($errors as  $error) {
+                echo "<div class='alert alert-danger'>$error</div>";
+            }
+           }else{
+            
+            $sql = "INSERT INTO users (full_name, email, password) VALUES ( ?, ?, ? )";
+            $stmt = mysqli_stmt_init($conn);
+            $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
+            if ($prepareStmt) {
+                mysqli_stmt_bind_param($stmt,"sss",$fullName, $email, $passwordHash);
+                mysqli_stmt_execute($stmt);
+                echo "<div class='alert alert-success'>Registro concluído com sucesso!</div>";
+            }else{
+                die("Something went wrong");
+            }
+           }
+          
+
+        }
+        ?>
+        <form action="registro.php" method="post">
+            <div class="form-group">
+                <input type="text" class="form-control" name="fullname" placeholder="Nome completo:">
+            </div>
+            <div class="form-group">
+                <input type="email" class="form-control" name="email" placeholder="Email:">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="Senha:">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="repeat_password" placeholder="Repetir senha:">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="cpf" placeholder="CPF:">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="telefone" placeholder="Telefone:">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="numCarteira" placeholder="Número da carteira do SUS:">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="endereco" placeholder="Endereço:">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="numero" placeholder="Número:">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="cidade" placeholder="Cidade:">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="estado" placeholder="Estado:">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="cep" placeholder="CEP:">
+            </div>
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" value="Register" name="submit">
+            </div>
+        </form>
+        <div>
+        <div><p>Já tem conta? <a href="login.php">Login</a></p></div>
+      </div>
+    </div>
+</body>
+</html>
