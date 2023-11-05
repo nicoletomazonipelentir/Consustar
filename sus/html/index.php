ae0f27e21d5572188f2646cac1afb1e57b11a45e
Renan Parise <renan.parise.ifc@gmail.com>
2023-11-05 18:53:33 -0300

    tfyft

--- sus/html/index.php
+++ sus/html/index.php
@@ -18,54 +18,5 @@ if (!isset($_SESSION["user"])) {
 <!--BOTAR O HEADER E O FOOTER-->
 <!--CSS inteiro pra isso não ficar esse nojo resto de xorume de lixo radioativo q ta agr-->
 <body>
-    <div id="areaTextoComBotao">
-        <h3>Uma solução para o agendamento de consultas online direcionado para o Sistema Único de Saúde (SUS)</h3>
-        <a id="botaoLogin" href="/sus/html/login.php" class="btn btn-primary disabled" tabindex="-1" role="button" aria-disabled="true">Login</a>
-        <a id="botaoCadastro" href="/sus/html/cadastro.php" class="btn btn-primary disabled" tabindex="-1" role="button" aria-disabled="true">Cadastre-se</a>
-        <!--Botar a logo do SUS tambem, talvez fora da div pra mexer so nela?-->
-    </div>
-
-    <div id="Tabela Horarios">
-        <center><table width=600 border=”1″>
-            <br><br>
-            <p><CENTER>Horários de Atendimento</P>
-            <br><br>
-            <tr>
-
-            </tr>
-            <tr><td>07:00</td><td><a>07:20</a></td><td><a>07:40
-            </a>
-
-            <tr>
-            <td><a>08:00</td></a></td><td><a>08:20</a></td></a></td><td><a>08:40</a></td>
-            </tr>  
-            
-            
-            </tr>
-            <tr><td>09:00</td><td><a>09:20</a></td><td><a>09:40
-            </a></td>
-            </tr>
-
-            <td><a>10:00</a></td><td><a>10:20
-            </a></td><td><a>10:40</a></td>
-            </tr>
-
-            <td><a>13:00</a></td><td><a>13:20</a></td><td>
-            <a>13:40</a></td>
-            </tr>
-
-            <td><a>14:00</a></td><td><a>
-            14:20</a></td><td>14:40</td>
-            </tr>
-
-            <tr><td><a>15:00</a></td><td><a>15:20</a></td><td>
-            <a>15:40</a></td>
-            </tr>
-
-            <td><a>16:00</a></td><td>
-            <a>16:20</a></td><td>16:40</td>
-            </tr>
-
-            </table></center>
-    </div>
+    <h1>bem vindo do login</h1>
 </body>
