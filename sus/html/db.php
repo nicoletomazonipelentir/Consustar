5447708943f9847d44cc63d908b3940324348e72
nicole <nicole.tpelentir@hotmail.com>
2023-11-26 18:39:22 -0300

    1

--- sus/html/db.php
+++ sus/html/db.php
@@ -60,10 +60,52 @@ function Cadastro($fullname,$email,$senha,$senhaRepeat,$telefone,$cpf,$numCartei
     $conn->close();
 }
 
+// function horariosVagos($dataAtual) {
+//     $conn = ConectaBD();
+//     $dataObj = DateTime::createFromFormat('d/m/Y', $dataAtual);
+//     $dataFormatada = $dataObj->format('Y-m-d');
+
+//     // Usando prepared statements para evitar SQL Injection
+//     $sql = "SELECT * FROM Horarios WHERE id = ?";
+//     $stmt = $conn->prepare($sql);
+//     $stmt->bind_param("s", $dataFormatada);
+//     $stmt->execute();
+    
+//     $result = $stmt->get_result();
+    
+//     if ($result->num_rows > 0) {
+//         while ($row = $result->fetch_assoc()) {
+
+//             if (compararHoras($row["horario"])) {
+//                 echo "<option value='" . $row["horario"] . "'>" . $row["horario"] . "</option>";
+//             }
+//         }
+//     } else {
+//         echo "Nenhum resultado encontrado.";
+//     }
+
+//     $conn->close();
+// }
+
+// function compararHoras($horaDoBanco) {
+//     $horaAtual = date("H:i");
+//     $horaDoBancoObjeto = DateTime::createFromFormat("H:i", $horaDoBanco);
+//     $horaAtualObjeto = DateTime::createFromFormat("H:i", $horaAtual);
+
+//     if ($horaAtualObjeto > $horaDoBancoObjeto) {
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+//se esse não funcionar, descoiziei o de cima
 function horariosVagos($dataAtual) {
     $conn = ConectaBD();
     $dataObj = DateTime::createFromFormat('d/m/Y', $dataAtual);
     $dataFormatada = $dataObj->format('Y-m-d');
+
+    // Obtenha a hora atual
+    $horaAtual = date("H:i:s");
 
     // Usando prepared statements para evitar SQL Injection
     $sql = "SELECT * FROM Horarios WHERE id = ?";
@@ -75,8 +117,14 @@ function horariosVagos($dataAtual) {
     
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
+            $horarioNoBanco = strtotime($row["horario"]);
+            $horaAtualTimestamp = strtotime($horaAtual);
 
-            if (compararHoras($row["horario"])) {
+            if ($dataFormatada == date("Y-m-d") && $horarioNoBanco > $horaAtualTimestamp) {
+                // Exibe apenas os horários que ainda não passaram para o dia de hoje
+                echo "<option value='" . $row["horario"] . "'>" . $row["horario"] . "</option>";
+            } elseif ($dataFormatada != date("Y-m-d")) {
+                // Exibe todos os horários para o dia seguinte
                 echo "<option value='" . $row["horario"] . "'>" . $row["horario"] . "</option>";
             }
         }
@@ -87,16 +135,12 @@ function horariosVagos($dataAtual) {
     $conn->close();
 }
 
-function compararHoras($horaDoBanco) {
-    $horaAtual = date("H:i");
-    $horaDoBancoObjeto = DateTime::createFromFormat("H:i", $horaDoBanco);
-    $horaAtualObjeto = DateTime::createFromFormat("H:i", $horaAtual);
 
-    if ($horaAtualObjeto > $horaDoBancoObjeto) {
-        return true;
-    }else{
-        return false;
-    }
+function compararHoras($horaDoBanco) {
+    $horaAtual = new DateTime();
+    $horaDoBancoObjeto = DateTime::createFromFormat("H:i", $horaDoBanco);
+
+    return $horaAtual > $horaDoBancoObjeto;
 }
 
 function pacientes($data, $horario,$email){
