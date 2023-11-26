94036db05c2a960510d326036b93afb72b059a9b
DavidMello01 <pessoal.david2307@gmail.com>
2023-11-26 11:57:23 -0300

    acessiblidade demonmiaca

--- sus/html/header.php
+++ sus/html/header.php
@@ -7,7 +7,7 @@
     <title>conSUStar</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    
+    <script src="https://cdn.userway.org/widget.js" data-account="SmEmT0uALN"></script>
 </head>
 <body>
     <header class="titulo">
